2dfe1f3d35c7db13bc88a57f2fcd3a321a9f579a
Shreemathinatarajan <shreemathinatarajan29@gmail.com>
2023-06-06 20:58:48 +0530

    charity

---
+++ Version 2 (Charity Miles)/src/app/app.module.ts
@@ -0,0 +1,63 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './Home/Home.component';
+import { CharityComponent } from './Charity/Charity.component';
+import { ContactComponent } from './Contact/Contact.component';
+import { AboutComponent } from './About/About.component';
+import { LoginComponent } from './Login/Login.component';
+import { RegisterComponent } from './Register/Register.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { PaymentComponent } from './payment/payment.component';
+import { AdminComponent } from './Admin/Admin.component';
+import { HeaderComponent } from './Header/Header.component';
+import { FooterComponent } from './Footer/Footer.component';
+import { AdminheaderComponent } from './Adminheader/Adminheader.component';
+import { AdmindashboardComponent } from './Admindashboard/Admindashboard.component';
+import { AdmineventComponent } from './Adminevent/Adminevent.component';
+import { AdminAddComponent } from './AdminAdd/AdminAdd.component';
+import { AdminMembershipComponent } from './AdminMembership/AdminMembership.component';
+import { AdminreviewComponent } from './Adminreview/Adminreview.component';
+import { AdminProfileComponent } from './AdminProfile/AdminProfile.component';
+import { AuthService } from './auth.service';
+
+@NgModule({
+  declarations: [																														
+    AppComponent,
+      HomeComponent,
+      CharityComponent,
+      ContactComponent,
+      AboutComponent,
+      LoginComponent,
+      RegisterComponent,
+      PaymentComponent,
+      AdminComponent,
+      HeaderComponent,
+      FooterComponent,
+      AdminComponent,
+      AdminheaderComponent,
+      AdmineventComponent,
+      AdminAddComponent,
+      AdmindashboardComponent,
+      AdminMembershipComponent,
+      AdminreviewComponent,
+      AdminProfileComponent
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+   
+  ],
+  providers: [
+    AuthService
+  ],
+  bootstrap: [AppComponent]
+  
+})
+export class AppModule { }
